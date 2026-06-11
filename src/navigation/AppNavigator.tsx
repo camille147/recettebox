8b2c7d1fba31728d2968f651859d216107cfa327
Camille <camille.pinault@coda-student.school>
2026-06-11 16:33:30 +0200

    fix(navigation): utilisation de HomeStack dans le tab et correction de la navigation

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,27 +1,60 @@
 import {NavigationContainer} from "@react-navigation/native";
-import {createNativeStackNavigator} from "@react-navigation/native-stack";
-import HomeScreen from "@/src/features/home/HomeScreen";
-import DetailsScreen from "@/src/features/details/DetailsScreen";
+import {Text, StyleSheet} from "react-native";
 import FavoriteScreen from "@/src/features/favorites/FavoriteScreen";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import HomeStack from "@/src/navigation/HomeStack";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
-
-
-export type RootStackParamList = {  // def des params pour chaque ecran
-    Home: undefined;
-    Detail: { idMeal: string };
-    Favorites: undefined;
+export type RootTabParamList = {
+    HomeTab: {
+        screen: 'Detail';
+        params: { idMeal: string }
+    } | undefined;
+    FavoritesTab: undefined;
 };
 
+export type HomeStackParamList = {
+    Home: undefined;
+    Detail: { idMeal: string };
+}
 
-const Stack = createNativeStackNavigator()
+const Tab = createBottomTabNavigator()
+
+
 export default function AppNavigator() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Home">
-                <Stack.Screen name="Home" component={HomeScreen}/>
-                <Stack.Screen name="Favorites" component={FavoriteScreen}/>
-                <Stack.Screen name="Detail" component={DetailsScreen}/>
-            </Stack.Navigator>
+            <Tab.Navigator screenOptions={{
+                tabBarActiveTintColor: '#e63946',
+                tabBarInactiveTintColor: '#888',
+                headerShown: false,
+                //tabBarBackground: () => (
+                //<BlurView tint="light" intensity={100} style={StyleSheet.absoluteFill} />
+                //),
+            }}>
+                <Tab.Screen name="HomeTab" component={HomeStack} options={{
+                    title: 'Recettes',
+                    tabBarIcon: ({ focused }) => (
+                        <Text style={{ fontSize: 20 }}>{focused ? <MaterialCommunityIcons name="book-open-variant" size={24} color="black" /> : <FontAwesome name="book" size={24} color="black" />}</Text>
+                    ),
+                }}/>
+                <Tab.Screen name="FavoritesTab" component={FavoriteScreen} options={{
+                    title: 'Favoris',
+                    tabBarIcon: ({ focused }) => (
+                        <Text style={{ fontSize: 20 }}>{focused ? <MaterialCommunityIcons name="notebook-heart" style={style.favoriteHeartRed} size={24} color="red" /> : <MaterialCommunityIcons name="notebook-heart-outline" size={24} color="black" />}</Text>
+                    ),
+                }}/>
+            </Tab.Navigator>
         </NavigationContainer>
     )
 }
+
+
+const style = StyleSheet.create(
+    {
+        favoriteHeartRed: {
+            color: '#AD0909'
+        }
+    }
+)
